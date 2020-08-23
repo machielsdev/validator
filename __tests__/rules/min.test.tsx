c9ff725cc1861aae795452b1a7fa441c4f2d8a35
Eran Machiels <dev@eranmachiels.nl>
2020-08-23 17:52:10 +0200

    Added min rule

---
+++ __tests__/rules/min.test.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ValidatorArea } from '../../src';
+import { ValidatorAreaProps } from '../../src/ValidatorArea';
+import min from '../../src/rules/min';
+
+describe('test min rule', () => {
+    it('should falsely validate input', () => {
+        const area = mount<ValidatorArea, ValidatorAreaProps>(
+            <ValidatorArea rules={[min(5)]}>
+                <input name="test" />
+            </ValidatorArea>
+        );
+
+        area.find('input').simulate('blur');
+        expect(area.state().errors.length).toBe(1);
+        expect(area.state().errors[0]).toBe('Test should be at least 5');
+    });
+
+    it('should falsely validate textarea', () => {
+        const area = mount<ValidatorArea, ValidatorAreaProps>(
+            <ValidatorArea rules={[min(5)]}>
+                <textarea name="test" />
+            </ValidatorArea>
+        );
+
+        area.find('textarea').simulate('blur');
+        expect(area.state().errors.length).toBe(1);
+        expect(area.state().errors[0]).toBe('Test should be at least 5');
+    });
+
+    it('should falsely validate select', () => {
+        const area = mount<ValidatorArea, ValidatorAreaProps>(
+            <ValidatorArea rules={[min(5)]}>
+                <select name="test">
+                    <option value="">Choose...</option>
+                </select>
+            </ValidatorArea>
+        );
+
+        area.find('select').simulate('blur');
+        expect(area.state().errors.length).toBe(1);
+        expect(area.state().errors[0]).toBe('Test should be at least 5');
+    });
+
+    it('should truly validate select', () => {
+        const area = mount<ValidatorArea, ValidatorAreaProps>(
+            <ValidatorArea rules={[min(5)]}>
+                <select name="test">
+                    <option value={5}>Choose...</option>
+                </select>
+            </ValidatorArea>
+        );
+
+        area.find('select').simulate('blur');
+        expect(area.state().errors.length).toBe(0);
+    });
+});
