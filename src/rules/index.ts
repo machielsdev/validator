3b896cc4d94547dd91ca09977e72e7e2d783e22a
Eran Machiels <dev@eranmachiels.nl>
2020-10-02 15:45:52 +0200

    Added everything to be exported

---
+++ src/rules/index.ts
@@ -0,0 +1,4 @@
+export { default as min } from '@/rules/min';
+export { default as max } from '@/rules/max';
+export { default as required } from '@/rules/required';
+export { IncorrectArgumentTypeError } from '@/rules/IncorrectArgumentTypeError';
