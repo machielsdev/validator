744f95f15006505c2acd28c6a82fc8ab4ed0cc25
Eran Machiels <dev@eranmachiels.nl>
2020-12-13 16:26:52 +0100

    Added active url to index export (#23)

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,4 +1,5 @@
 export { default as min } from '@/rules/min';
 export { default as max } from '@/rules/max';
 export { default as required } from '@/rules/required';
+export { default as activeUrl } from '@/rules/activeUrl';
 export { IncorrectArgumentTypeError } from '@/rules/IncorrectArgumentTypeError';
